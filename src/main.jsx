c0dee27923c922024450752a07697a921fa179d9
JungGo <khatawut.ptk@gmail.com>
2026-03-10 15:06:19 +0700

    Refactor code structure for improved readability and maintainability

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ConfigProvider>
+  <ConfigProvider
+  theme={{
+      token: {
+        colorBgLayout: 'transparent',
+        fontFamily: 'Prompt, sans-serif',
+      },
+    }}
+  >
     <App />
   </ConfigProvider>
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,12 +5,11 @@ import {
     Form, 
     Input, 
     Button, 
-    Typography 
+    Typography
 } from 'antd'
 import { 
     UserOutlined, 
-    LockOutlined, 
-    LoginOutlined 
+    LockOutlined 
 } from '@ant-design/icons'
 
 const { Title, Text } = Typography
@@ -18,29 +17,45 @@ const { Title, Text } = Typography
 const Login = () => {
   return (
         <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-            <Card style={{ width: 360, borderRadius: 12, boxShadow: '0 4px 30px rgba(80, 79, 79, 0.2)' }}>
-                <div style={{ textAlign: 'center', marginBottom: 24 }}>
-                    <Title level={3} style={{ marginBottom: 2 }}>ระบบติดตามยืม-คืนอุปกรณ์</Title>
-                    <Title level={5} style={{ color: '#666' }}>กรุณาเข้าสู่ระบบเพื่อใช้งาน</Title>
-                </div>
-                <Form layout="vertical" onFinish={(values) => console.log('Login values:', values)}>
-                    <Form.Item name="username" rules={[{ required: true }]}>
-                        <Input prefix={<UserOutlined />} size="large" placeholder="Username" />
-                    </Form.Item>
-                    <Form.Item name="password" rules={[{ required: true }]}>
-                        <Input.Password prefix={<LockOutlined />} size="large" placeholder="Password" />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" size="large" block icon={<LoginOutlined />} loading={false}>
-                            เข้าสู่ระบบ
-                        </Button>
-                    </Form.Item>
-                </Form>
-                <div style={{ textAlign: 'center', marginTop: 12, display: 'flex', flexDirection: 'column' }}>
-                    <Text style={{ color: '#666', marginTop: 12, fontSize: 12 }}>Resource Tracking & Checkout System 2025 © 2025</Text>
-                    <Text style={{ color: '#666', marginTop: 12, fontSize: 12 }}>ver. 1.0.0</Text>
-                </div>
-            </Card>
+            <div style={{ display: 'flex', width: '80%', maxWidth: 1000, alignItems: 'stretch' }}>
+                <Card style={{ flex: 1, height: 400, borderRadius: 12, boxShadow: '0 4px 30px rgba(80, 79, 79, 0.2)', display: 'flex' }}>
+
+                    <div style={{ flex: 1, padding: 24 }}>
+                        <div style={{ textAlign: 'center', marginBottom: 24 }}>
+                            <Title level={3} style={{ marginBottom: 2 }}>ระบบติดตามยืม-คืนอุปกรณ์</Title>
+                            <Title level={5} style={{ color: '#666' }}>ระบบติดตามโครงการ MOA</Title>
+                        </div>
+                        <Form layout="vertical" onFinish={(values) => console.log('Login values:', values)}>
+                            <Form.Item name="username" rules={[{ required: true }]}>
+                                <Input prefix={<UserOutlined />} size="large" placeholder="Username" />
+                            </Form.Item>
+                            <Form.Item name="password" rules={[{ required: true }]}>
+                                <Input.Password prefix={<LockOutlined />} size="large" placeholder="Password" />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" size="large" block  loading={false}>
+                                    เข้าสู่ระบบ
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                        <div style={{ textAlign: 'center', marginTop: 12, display: 'flex', flexDirection: 'column' }}>
+                            <Text style={{ color: '#7f7c7c', marginTop: 5, fontSize: 10 }}>@ 2026 | ระบบติดตามโครงการ MOA ver. 1.0.0</Text>
+                            <Text style={{ color: '#7f7c7c', marginTop: 5, fontSize: 10 }}>บริษัท อาร์คติค เทคนิคอล จำกัด</Text>
+                        </div>
+                    </div>
+                    {/* right side: inside card image */}
+                    <div
+                        style={{
+                            flex: 1,
+                            height: '100%',
+                            borderRadius: '0 12px 12px 0',
+                            backgroundImage: 'url(../public/water.jpg)',
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center'
+                        }}
+                    />
+                </Card>
+            </div>
         </div>
     );
 }
